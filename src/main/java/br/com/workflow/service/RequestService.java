4444dfdd06c4b4010d51deadb8ad837b61520e73
Leonardo Brito <leonardobrito371@gmail.com>
2026-09-11 20:10:44 -0300

    Criar e recuperar request2

--- src/main/java/br/com/workflow/service/RequestService.java
+++ src/main/java/br/com/workflow/service/RequestService.java
@@ -1,10 +1,31 @@
 package br.com.workflow.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.workflow.dto.RequestDTO;
+import br.com.workflow.entity.Request;
+import br.com.workflow.mapper.RequestMapper;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class RequestService {
-    public String getMessage(){
-        return "Workflow Service is Running";
+
+    public List<Request> requests = new ArrayList<>();
+    @Inject 
+    private RequestMapper mapper;
+
+    public List<Request> getRequests(){
+        return requests;
+    }
+
+    public Boolean createtRequest(RequestDTO request){
+
+        Request request1 = mapper.toEntity(request);
+
+        requests.add(request1);
+        
+        return true;
     }
 }
