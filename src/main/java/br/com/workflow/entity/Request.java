36869fd00d70c9b25855aa895997d269f8429f65
Leonardo Brito <leonardobrito371@gmail.com>
2026-09-11 20:09:26 -0300

    Criar e recuperar request

---
+++ src/main/java/br/com/workflow/entity/Request.java
@@ -0,0 +1,9 @@
+package br.com.workflow.entity;
+
+public class Request {
+    public int id;
+    public String title;
+    public String description;
+    public RequestStatus status;
+    public String createdBy;
+}
