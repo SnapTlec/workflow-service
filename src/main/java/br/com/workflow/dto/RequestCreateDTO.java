4cbfb96a84a2b7e85d3488be42a7837945eccfc0
Leonardo Brito <leonardobrito371@gmail.com>
2026-09-20 18:11:35 -0300

    Fix: Cria os getters e setters devido erro na classe RequestMapper.

--- src/main/java/br/com/workflow/dto/RequestCreateDTO.java
+++ src/main/java/br/com/workflow/dto/RequestCreateDTO.java
@@ -2,7 +2,26 @@ package br.com.workflow.dto;
 
 
 public class RequestCreateDTO {
-    public String title;
-    public String description;
-    public String createdBy;
+    private String title;
+    private String description;
+    private String createdBy;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getCreatedBy() {
+        return createdBy;
+    }
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
 }
